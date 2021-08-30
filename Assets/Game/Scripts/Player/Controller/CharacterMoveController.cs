c525df6c41fa0c683da68a91f42d789bb6b10440
SovietChan <52289632+SovietChan@users.noreply.github.com>
2021-08-30 22:05:09 +0700

    build: Player Move Animation

--- Assets/Game/Scripts/Player/Controller/CharacterMoveController.cs
+++ Assets/Game/Scripts/Player/Controller/CharacterMoveController.cs
@@ -8,24 +8,71 @@ public class CharacterMoveController : MonoBehaviour
     public float moveAccel;
     public float maxSpeed;
 
+    [Header("Jump")]
+    public float jumpAccel;
+
+    private bool isJumping;
+    private bool isOnGround;
+
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
     private Rigidbody2D rig;
+
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isOnGround)
+            {
+                isJumping = true;
+            }
+        }
+        anim.SetBool("isOnGround", isOnGround);
     }
 
     private void FixedUpdate()
     {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+       
+        
+
+        
+        if (hit)
+        {
+            if(!isOnGround&& rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
         Vector2 velocityVector = rig.velocity;
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+
         velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
         rig.velocity = velocityVector;
 
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down * groundRaycastDistance), Color.white);
     }
 }
